1ff5edd0c7d9c1490d9ba9061764e75c6d7afca3
Marlon Baptista de Quadros <marlonbquadros@gmail.com>
2018-01-09 11:31:34 -0200

    - Correção nas configurações do dockerfile que impedia a utilização do docker

--- docker/run_scrapy_news.sh
+++ docker/run_scrapy_news.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# wait for PSQL server to start
+# wait for Redis and Mongo servers to start
 sleep 10
 
 # Comando para rodar o script.
--- news_crawler/settings.py
+++ news_crawler/settings.py
@@ -14,10 +14,9 @@ BOT_NAME = 'news_crawler'
 SPIDER_MODULES = ['news_crawler.spiders']
 NEWSPIDER_MODULE = 'news_crawler.spiders'
 
-LOG_LEVEL = 'DEBUG'
+LOG_LEVEL = 'WARNING'
 
 DOWNLOAD_DELAY = 1
-
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 # USER_AGENT = 'news_crawler (+http://www.yourdomain.com)'
@@ -79,23 +78,24 @@ MONGODB_COLLECTION = "articles"
 DUPEFILTER_CLASS = "scrapy_redis.dupefilter.RFPDupeFilter"
 SCHEDULER = "scrapy_redis.scheduler.Scheduler"
 SCHEDULER_PERSIST = True
-
+REDIS_HOST = 'redis'
+REDIS_PORT = 6379
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
 # NOTE: AutoThrottle will honour the standard settings for concurrency and delay
-AUTOTHROTTLE_ENABLED=True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-AUTOTHROTTLE_START_DELAY=5
+AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-AUTOTHROTTLE_MAX_DELAY=60
+AUTOTHROTTLE_MAX_DELAY = 60
 # Enable showing throttling stats for every response received:
-AUTOTHROTTLE_DEBUG=True
+AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-HTTPCACHE_ENABLED=True
-HTTPCACHE_EXPIRATION_SECS=0
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = 0
 # HTTPCACHE_DIR='httpcache'
 # HTTPCACHE_IGNORE_HTTP_CODES=[]
-HTTPCACHE_STORAGE='scrapy.extensions.httpcache.DbmCacheStorage'
+HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.DbmCacheStorage'
